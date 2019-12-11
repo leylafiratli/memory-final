4468ac18aa2c804c7ff725b8c2628251c9c0c6ee
Leyla Firatli <leylafiratli@leylas-mbp.home>
2019-12-11 11:51:11 -0800

    server

--- src/global.js
+++ src/global.js
@@ -1,2 +1,2 @@
-export const server = 'localhost:3000';
-//export const server = 'https://memoryjournalapp.herokuapp.com';
+//export const server = 'localhost:3000';
+export const server = 'https://firatli-memory.herokuapp.com';
